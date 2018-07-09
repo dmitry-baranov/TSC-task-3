a2edc77940548db2477ab74c976f57def3215839
DBARANOV <dmitry-baranov>
2018-07-09 16:04:08 +0300

    hello

---
+++ src/main/java/logic/InverseSemaphore.java
@@ -0,0 +1,25 @@
+package logic;
+
+public class InverseSemaphore {
+    private volatile int value = 0;
+    private Object lock = new Object();
+
+    public void beforeSubmit() {
+        synchronized(lock) {
+            value++;
+        }
+    }
+
+    public void taskCompleted() {
+        synchronized(lock) {
+            value--;
+            if (value == 0) lock.notifyAll();
+        }
+    }
+
+    public void awaitCompletion() throws InterruptedException {
+        synchronized(lock) {
+            while (value > 0) lock.wait();
+        }
+    }
+}
